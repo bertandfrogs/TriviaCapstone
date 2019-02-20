b12fb1938393727cf9470fbfc3facb729eedb8ff
Chad Baxter <chaddudeguy@gmail.com>
2019-02-20 08:19:31 -0700

    add firestore and firebase hosting to project

---
+++ src/api-keys.ts
@@ -0,0 +1,10 @@
+export const APIKeys = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAU6Ii4Pa5OwSUL3aBE8rOZJ61DNJsyGwE',
+    authDomain: 'triviacapstone.firebaseapp.com',
+    databaseURL: 'https://triviacapstone.firebaseio.com',
+    projectId: 'triviacapstone',
+    storageBucket: 'triviacapstone.appspot.com',
+    messagingSenderId: ''
+  }
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { APIKeys } from '../api-keys';
+import { AngularFireModule } from 'angularfire2'
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
